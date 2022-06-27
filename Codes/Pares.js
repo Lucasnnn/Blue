4f0b3f1ce90b80243cef57d7e3c9102940b64011
Lucas de Oliveira Santos <106331145+Lucasnnn@users.noreply.github.com>
2022-06-27 12:05:57 -0300

    main

--- Codes/Pares.js
+++ Codes/Pares.js
@@ -1,6 +1,25 @@
 const prompt = require('prompt-sync')()
-const numero = +prompt('digite um valor:')
+console.log()
+let quanti = prompt('Quantos numeros quer que eu organize e compare entre pares e impares ?')
+let par = []
+let impar = []
 
-resto = numero % 2
-par = resto == 0
-if (par) { console.log("O numero " + numero + " é par") } else { console.log("O numero " + numero + " é ímpar") }
+for(let i = 0; i < quanti;){
+    console.log();
+    let per = +prompt(`Me informe ${quanti} numeros inteiros por favor :`); 
+    console.clear();
+ if(per/per != 1 ){console.log('Digite apenas numeros') ; i--}
+  else if (per % 2 == 0){par.push(per)} 
+   else if(per % 2 != 0){impar.push(per)}
+    else if (per % 1 != 0){console.log("Insira numeros inteiros por favor ")}
+i++
+    
+}
+console.clear();
+
+console.log("Pares : " + par);
+console.log()
+console.log('Impares :' +  impar);
+console.log()
+console.log('Todos :' + par + impar);
+console.log();
