2c9f5cb71c2320e7854ce9d1c772f82fb8202363
Lucasnnn <olucas58@gmail.com>
2022-08-15 20:27:47 -0300

    origin/main

--- M3/Projetos/Crud-Carros/Carros-Crud/index.js
+++ M3/Projetos/Crud-Carros/Carros-Crud/index.js
@@ -9,9 +9,20 @@ const app = express();
 connectToDatabase();
 
 app.use(express.json());
-app.use(cors());
+
+app.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header('Access-Control-Allow-Origin', '*');
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
+
 app.use('/carros', routes);
 
 app.listen(port, () => {
-  console.log(`Servidor Bugando na porta http://localhost:${port}`);
+  console.log(
+    `Servidor Bugando na porta http://localhost:${port}/carros/todosCarros`,
+  );
 });
