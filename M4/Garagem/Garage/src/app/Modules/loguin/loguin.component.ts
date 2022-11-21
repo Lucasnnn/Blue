b44d66b60c2b66a684b40068cff9ff1fded1b39a
Lucasnnn <olucas58@gmail.com>
2022-11-20 23:38:28 -0300

    ajuste

--- M4/Garagem/Garage/src/app/Modules/loguin/loguin.component.ts
+++ M4/Garagem/Garage/src/app/Modules/loguin/loguin.component.ts
@@ -4,6 +4,7 @@ import {
   OnInit,
   ViewEncapsulation,
 } from '@angular/core';
+import { AppService } from 'src/app/app.service';
 import { CarrosType } from 'src/app/models/carros.type';
 import SwiperCore, { Autoplay } from 'swiper';
 
@@ -17,56 +18,7 @@ SwiperCore.use([Autoplay]);
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoguinComponent implements OnInit {
-  carros: Array<CarrosType> = [
-    {
-      nome: 'Audi A3',
-      ano: '2018',
-      potencia: '300',
-      tipo: 'Carro',
-      linkFoto:
-        'https://i.pinimg.com/564x/a5/3d/4b/a53d4bd3653c85de11880fb1d37eac64.jpg',
-    },
-    {
-      nome: 'Bmw M3 touring',
-      ano: '2022',
-      potencia: '510',
-      tipo: 'Carro',
-      linkFoto:
-        'https://www.automaistv.com.br/wp-content/uploads/2022/06/P90468211_highRes_the-first-ever-bmw-m_edited-990x594.jpg',
-    },
-    {
-      nome: 'H2R',
-      ano: '2022',
-      potencia: '310',
-      tipo: 'Moto',
-      linkFoto:
-        'https://i.pinimg.com/564x/06/19/e7/0619e7c35dd9b6af8a18f53d22b16ce0.jpg',
-    },
-    {
-      nome: 'Audi A3',
-      ano: '2018',
-      potencia: '300',
-      tipo: 'Carro',
-      linkFoto:
-        'https://i.pinimg.com/564x/a5/3d/4b/a53d4bd3653c85de11880fb1d37eac64.jpg',
-    },
-    {
-      nome: 'Bmw M3 touring',
-      ano: '2022',
-      potencia: '510',
-      tipo: 'Carro',
-      linkFoto:
-        'https://www.automaistv.com.br/wp-content/uploads/2022/06/P90468211_highRes_the-first-ever-bmw-m_edited-990x594.jpg',
-    },
-    {
-      nome: 'H2R',
-      ano: '2022',
-      potencia: '310',
-      tipo: 'Moto',
-      linkFoto:
-        'https://i.pinimg.com/564x/06/19/e7/0619e7c35dd9b6af8a18f53d22b16ce0.jpg',
-    },
-  ];
+  carros: Array<CarrosType>;
 
   swiperConfig: any = {
     slidesPerView: 1,
@@ -126,7 +78,9 @@ export class LoguinComponent implements OnInit {
     },
   };
 
-  constructor() {}
+  constructor(private service: AppService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.service.garagem$.subscribe((res) => (this.carros = res));
+  }
 }
