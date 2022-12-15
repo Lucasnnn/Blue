5a43634dd3104bcdfd8142d61da2dc0d53a82950
Lucas de Oliveira Santos <lucasn3de@gmail.com>
2022-12-14 23:14:15 -0300

    att

--- M5/Projeto  2/garageApi/src/app.module.ts
+++ M5/Projeto  2/garageApi/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { CarsModule } from './modules/cars/cars.module';
 import { ProfileModule } from './modules/profile/profile.module';
 import { UsersController } from './modules/users/users.controller';
 import { UsersService } from './modules/users/users.service';
 import { DatabaseModule } from './prisma/database.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, ProfileModule],
+  imports: [DatabaseModule, AuthModule, ProfileModule , CarsModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
